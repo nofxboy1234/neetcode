b4e6ea03a0e634799c825da4de19393951869f2e
Dylan Palmboom <nofxboy1234@gmail.com>
2025-06-25 09:33:57 +0200

    Update ContainsDuplicate

---
+++ Arrays-Hashing/ContainsDuplicate.js
@@ -0,0 +1,16 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    //
+  }
+}
+
+{
+  const solution = new Solution();
+  const nums = [1, 2, 3, 3];
+  const result = solution.hasDuplicate(nums);
+  console.log(result);
+}
